9a6c543decd1ee81c3a11ae5aa66ccfeaba7e9e4
basstianVR <sebas.varela.ruiz@hotmail.com>
2025-05-16 23:18:27 -0500

    feat: Add locators for InfoItemPage

--- pages/info_item_page.py
+++ pages/info_item_page.py
@@ -3,3 +3,4 @@ from screenpy_selenium import Target
 
 class InfoItemPage:
     BUTTON_ADD_TO_CART = Target.the("button to add an item to the cart").located_by((By.XPATH, "//a[@class='btn btn-success btn-lg']"))
+    TITLE_ITEM = Target.the("Title of the item selected").located_by((By.XPATH, "//h2[@class='name']"))
