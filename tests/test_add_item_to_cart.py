e1cbbba297a26b48f10645b3e4ce191b52bb2dae
basstianVR <sebas.varela.ruiz@hotmail.com>
2025-05-16 23:31:31 -0500

    Finish implementation of test_add_item_to_cart

--- tests/test_add_item_to_cart.py
+++ tests/test_add_item_to_cart.py
@@ -1,13 +1,24 @@
+import pytest
 from actors.user import create_user
 from tasks.login import LogIn
 from tasks.add_to_cart import AddItemToCart
+from tasks.select_item import SelectItem
+from tasks.go_to_cart import GoToCart
+from tasks.delete_item import DeleteItem
 from screenpy_selenium.questions import Text
 from screenpy_selenium.actions import Pause
 from pages.navbar import NavBar
-from screenpy import See
+from pages.info_item_page import InfoItemPage
+from pages.cart_page import CartPage
+from screenpy import Eventually, See
 from screenpy.resolutions import ContainsTheText
 
-def test_user_can_log_in():
+@pytest.mark.parametrize("product_name", [
+    "Samsung galaxy s6",
+    "Nokia lumia 1520"
+])
+
+def test_user_can_log_in(product_name):
     Sebas = create_user()
     
     Sebas.attempts_to(
@@ -19,7 +30,25 @@ def test_user_can_log_in():
     )
 
     Sebas.attempts_to(
-        AddItemToCart("Samsung galaxy s6")
+        SelectItem(product_name)
+    )
+
+    Sebas.should(
+        Eventually(See.the(Text.of(InfoItemPage.TITLE_ITEM), ContainsTheText(product_name)))
+    )
+
+    Sebas.attempts_to(
+        AddItemToCart(),
+        GoToCart(),
+    )
+
+    Sebas.should(
+        Eventually(See.the(Text.of(CartPage.LAST_ITEM_ADDED), ContainsTheText(product_name)))
+    )
+
+    Sebas.attempts_to(
+        DeleteItem(CartPage.DELETE_BUTTON),
+        Pause.for_(2).seconds_because("need to see")
     )
     
 
