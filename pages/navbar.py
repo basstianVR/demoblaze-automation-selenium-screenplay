4a5e854c74662741270b60e79b215e0ca28e32eb
basstianVR <sebas.varela.ruiz@hotmail.com>
2025-05-19 16:23:31 -0500

    Add additional locators

--- pages/navbar.py
+++ pages/navbar.py
@@ -6,3 +6,7 @@ class NavBar:
     WELCOME_FIELD = Target.the("welcome field").located_by((By.XPATH, "//a[@id='nameofuser']"))
     WELCOME_MESSAGE = Target.the("welcome message").located((By.ID, "nameofuser"))
     CART_BUTTON = Target.the("button to the cart").located_by((By.XPATH, "//a[normalize-space()='Cart']"))
+    CONTACT_BUTTON = Target.the("button to the contact form").located_by((By.XPATH, "//a[normalize-space()='Contact']"))
+    ABOUT_US_BUTTON = Target.the("button to display the about us section")
+    LOG_OUT_BUTTON = Target.the("button to log out").located_by((By.XPATH, "//a[@id='logout2']"))
+    HOME_BUTTON = Target.the("button to go to home section").located_by((By.XPATH, "//li[@class='nav-item active']//a[@class='nav-link']"))
