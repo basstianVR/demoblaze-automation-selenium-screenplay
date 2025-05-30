ea070a1d7dc337a795cc195434f42a517e683a1e
basstianVR <sebas.varela.ruiz@hotmail.com>
2025-05-30 14:49:49 -0500

    Add new logout functionality and test

---
+++ tasks/log_out.py
@@ -0,0 +1,12 @@
+from screenpy import Actor
+from screenpy_selenium.actions import Click
+from pages.navbar import NavBar
+
+class LogOut:
+    def __init__(self):
+        pass
+
+    def perform_as(self, actor:Actor):
+        return actor.attempts_to(
+            Click.on(NavBar.LOG_OUT_BUTTON)
+        )
---
+++ tests/test_log_out.py
@@ -0,0 +1,26 @@
+from actors.user import create_user
+from tasks.login import LogIn
+from tasks.log_out import LogOut
+from screenpy_selenium.questions import Text
+from pages.navbar import NavBar
+from screenpy import See, Eventually
+from screenpy.resolutions import ContainsTheText
+
+def test_log_out():
+    Sebas = create_user()
+
+    Sebas.attempts_to(
+        LogIn("Darkan", "pwd1234")
+    )
+
+    Sebas.should(
+        See.the(Text.of(NavBar.WELCOME_MESSAGE), ContainsTheText("Welcome Darkan"))
+    )
+
+    Sebas.attempts_to(
+        LogOut()
+    )
+
+    Sebas.should(
+        Eventually(See.the(Text.of(NavBar.LOGIN_NAV_BUTTON), ContainsTheText("Log in")))
+    )
