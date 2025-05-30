dd86d0f16458b2c09f04fb5289323a87fea6caa6
basstianVR <sebas.varela.ruiz@hotmail.com>
2025-05-30 14:52:53 -0500

    Clean up: remove unused imports from test

--- tests/test_send_contact_message.py
+++ tests/test_send_contact_message.py
@@ -1,10 +1,6 @@
 from actors.user import create_user
 from tasks.login import LogIn
 from tasks.send_message import SendMessage
-from screenpy_selenium.questions import Text
-from pages.navbar import NavBar
-from screenpy import See
-from screenpy.resolutions import ContainsTheText
 
 def test_send_contact_message():
     Sebas = create_user()
